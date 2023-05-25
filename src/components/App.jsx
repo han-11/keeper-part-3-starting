b5ff82e39da33b6aec2da6aaada99269ea7c737d
han-11 <heather.wang@lincolnuni.ac.nz>
2023-05-25 16:29:01 +1200

    version 1.1.0

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
+
+
+
 function App() {
+
+  const [notes, setNotes] = useState([]);
+
+  function addNote(note){
+     setNotes((prevNotes) => {
+      return [...prevNotes, note];
+    })
+  }
+
+  function handleDelete(id){
+    setNotes((allNotes) =>
+      {
+        return allNotes.filter((item,index)=>{
+          return index !== id;
+        })
+      }
+    )
+  }
+
   return (
     <div>
       <Header />
-      <CreateArea />
-      <Note key={1} title="Note title" content="Note content" />
+
+      <CreateArea onAdd={addNote} />
+
+
+      {notes.map((singleNote, index)=>{ 
+       return <Note 
+       key = {index}
+       id={index}
+      title={singleNote.title}
+      content={singleNote.content}
+      onDelete={handleDelete}
+      />  
+       } )}
+      
+  
       <Footer />
     </div>
   );
